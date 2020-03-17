8b2912d40db9905b9fd7616bbedd63f5202c086e
Devon Gadarowski <devon.gadarowski@knights.ucf.edu>
2020-03-17 12:19:40 -0400

    Worked on login widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,176 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'dart:ui';
+import 'package:http/http.dart' as http;
+
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatelessWidget
+{
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context)
+  {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Mango',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        inputDecorationTheme: InputDecorationTheme(
+          border: UnderlineInputBorder(borderRadius: BorderRadius.circular(10)),
+          fillColor: Color(0xFF75A544),
+          counterStyle: TextStyle(
+            color: Colors.white
+          ),
+          hintStyle: TextStyle(
+            color: Colors.white
+          ),
+          errorStyle: TextStyle(
+            color: Colors.red
+          )
+        ),
+        buttonTheme: ButtonThemeData(
+          buttonColor: Color(0xFF75A544),
+          textTheme: ButtonTextTheme.primary
+        ),
+        primarySwatch: Colors.orange,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MangoHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
+class MangoTitle extends StatelessWidget
+{
+  Widget build(BuildContext context)
+  {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text("mango", style: TextStyle(
+          color: Colors.black,
+          fontFamily: "Nunito",
+          fontSize: 32,
+          fontWeight: FontWeight.bold
+          )
+        ),
+        Flexible(child: SizedBox(width: 5)),
+        SizedBox(height: 32, child: Image(image: AssetImage('assets/mango.png'), fit: BoxFit.scaleDown))
+      ]
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class MangoHomePage extends StatelessWidget
+{
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+      body: FractionallySizedBox(
+        heightFactor: 1,
+        widthFactor: 1,
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Color(0xFFFFD151), Color(0xFFFF8C42)]
+            )
+          ),
+          child: MangoLoginBox()
+        )
+      )
+    );
+  }
+}
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class MangoLoginBox extends StatefulWidget
+{
+  MangoLoginState createState()
+  {
+    return MangoLoginState();
+  }
+}
+
+class MangoLoginState extends State<MangoLoginBox>
+{
+  final _loginKey = GlobalKey<FormState>();
+  String email = '';
+  String password = '';
+
+  // https://flutter.dev/docs/cookbook/networking/fetch-data
+  void doLogin() async
+  {
+    if (_loginKey.currentState.validate())
+    {
+      Scaffold.of(context).showSnackBar(SnackBar(content: Text('Logging In')));
+      //var response = await http.post("https://mangogreenhouse.com/api/login", body: {'email': this.email, 'password': this.password});
+    }
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+  // https://flutter.dev/docs/cookbook/forms/validation
+  // https://medium.com/flutter-community/realistic-forms-in-flutter-part-1-327929dfd6fd
+  Widget build(BuildContext context)
+  {
+    return FractionallySizedBox(
+      heightFactor: 0.6,
+      widthFactor: 0.8,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10)
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        child: Form(
+          key: _loginKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+              FractionallySizedBox(
+                child: MangoTitle()
+              ),
+              Flexible(child: FractionallySizedBox(heightFactor: 0.3)),
+              FractionallySizedBox(
+                widthFactor: 0.85,
+                child: TextFormField(
+                  decoration: InputDecoration(filled: true, hintText: "email"),
+                  validator: (value) {
+                    if (value.isEmpty)
+                    {
+                      return 'password invalid';
+                    }
+
+                    return null;
+                  },
+                  onSaved: (val) => setState(() => this.email = val)
+                )
+              ),
+              Flexible(child: FractionallySizedBox(heightFactor: 0.2)),
+              FractionallySizedBox(
+                widthFactor: 0.85,
+                child: TextFormField(
+                  decoration: InputDecoration(filled: true, hintText: "password"),
+                  validator: (value) {
+                    if (value.isEmpty)
+                    {
+                      return 'password invalid';
+                    }
+
+                    return null;
+                  },
+                  onSaved: (val) => setState(() => this.password = val),
+                )
+              ),
+              Flexible(child: FractionallySizedBox(heightFactor: 0.2)),
+              FractionallySizedBox(
+                child: RaisedButton(
+                  onPressed: () { doLogin(); },
+                  child: Text("Log In", style: TextStyle(color: Colors.white))
+                )
+              )
+            ]
+          )
+        )
+      )
     );
   }
 }
